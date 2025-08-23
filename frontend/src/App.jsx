2a7558f2d058ee38e83f99992cd8c569e98e1878
XxyarcexX <jjhernandezyarce@gmail.com>
2025-08-23 18:33:44 -0500

    botones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,41 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import {
+  ViewButton,
+  EditButton,
+  DeleteButton,
+  CancelButton,
+  SaveButton,
+  CloseButton,
+  DeleteTextButton,
+  EditTextButton,
+  ExportExcelButton,
+  ExportPDFButton
 
-function App() {
+} from "./shared/buttons";
 
-
+export default function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-    </h1>
-    </>
-  )
-}
+    <div className="flex flex-col items-center gap-6 p-6">
+      <div className="flex gap-4">
+        <ViewButton />
+        <EditButton />
+        <DeleteButton />
+      </div>
 
-export default App
+      <div className="flex gap-4">
+        <CancelButton />
+        <SaveButton />
+        <DeleteTextButton />
+        <EditTextButton />
+      </div>
+
+      <div className="mt-4">
+        <CloseButton />
+      </div>
+      <div className="flex gap-4">
+        <ExportExcelButton />
+        <ExportPDFButton />
+    </div>
+    </div>
+  );
+}
