eaf777528f9da6e8e41dcbfa92b2d6d647b24c1e
unknown <Sena@MEDAPRCSGFSD791.FormacionSena.RED>
2025-08-28 11:15:00 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,21 +11,24 @@ import {
   EditTextButton,
   ExportExcelButton,
   ExportPDFButton
-
-import './App.css'
-import Sidebar from './shared/sidebar.jsx'
-
-function App() {
-
-} from "./shared/buttons";
-
+} from "./shared/buttons.jsx";
+// import './App.css'
+import Sidebar from './shared/sidebar.jsx';
 export default function App() {
   return (
 
     <>
     <Sidebar />
+    {/* <ViewButton />
+    <EditButton />
+    <DeleteButton />
+    <CancelButton />
+    <SaveButton />
+    <CloseButton />
+    <DeleteTextButton />
+    <EditTextButton />
+    <ExportExcelButton />
+    <ExportPDFButton /> */}
     </>
   )
-}
-  );
 }
