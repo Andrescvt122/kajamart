59fd96900897cbd4058d4222c54537c334a70d0e
Andrescvt122 <{{your_github_email}}>
2025-08-29 22:59:29 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import IndexSuppliers from './features/suppliers/indexSuppliers.jsx'
 import Sidebar from './shared/sidebar.jsx'
+import IndexClientReturns from './features/returns/indexClientReturns.jsx'
 
 export default function App() {
   return (
       <Routes>
         <Route path="/" element={<Sidebar/>} />
         <Route path="/suppliers" element={<IndexSuppliers />} />
+        <Route path='/returns/clients' element={<IndexClientReturns/>} />
       </Routes>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import Footer from './components/footerComponent'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
