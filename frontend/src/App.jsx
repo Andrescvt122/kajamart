d78afcdf30708b516cfbc70d8c7acc33aae26e05
XxyarcexX <jjhernandezyarce@gmail.com>
2025-08-31 17:53:37 -0500

    actualizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import IndexSuppliers from './features/suppliers/indexSuppliers.jsx'
-import Sidebar from './shared/sidebar.jsx'
+// App.jsx
+import { Routes, Route } from "react-router-dom";
+import IndexSuppliers from "./features/suppliers/indexSuppliers.jsx";
+import IndexCategories from "./features/Categories/indexCategories.jsx";
+import Sidebar from "./shared/sidebar.jsx";
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Sidebar/>} />
-        <Route path="/suppliers" element={<IndexSuppliers />} />
-      </Routes>
-  )
-}
+    <div className="flex min-h-screen">
+      <Sidebar /> {/* <- fijo, siempre cargado */}
 
+      <div className="flex-1">
+        <Routes>
+          <Route path="/suppliers" element={<IndexSuppliers />} />
+          <Route path="/categories" element={<IndexCategories />} />
+          <Route path="/" element={<h1 className="p-8">Inicio</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
