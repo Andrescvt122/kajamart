f83e63561e96ceac265b1aebe012a3c0f07ec95e
unknown <Sena@MEDAPRCSGFSD791.FormacionSena.RED>
2025-09-01 10:36:28 -0500

    ola

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,14 +2,13 @@ import react from "react";
 import { Routes, Route } from "react-router-dom";
 import IndexSuppliers from "./features/suppliers/indexSuppliers";
 import IndexClientReturns from "./features/returns/indexClientReturns";
-import IndexCategories from "./features/Categories/indexCategories";
-import FooterComponent from "./components/footerComponent";
+import IndexCategories from "./features/categories/indexCategories";
 export default function RoutesAdmin() {
   return (
     <>
       <Routes>
         <Route path="/suppliers" element={<IndexSuppliers />} />
-        <Route path="/categories" element={<FooterComponent />} />
+        <Route path="/categories" element={<IndexCategories />} />
         <Route path="/returns/clients" element={<IndexClientReturns />} />
         <Route path="/" element={<h1 className="p-8">Inicio</h1>} />
       </Routes>
