26b828ee483ffac51ec5294111a893dcd9cde771
karito132006 <karollceballos9@gmail.com>
2025-09-08 07:26:16 -0600

    1

--- frontend/src/features/clients/indexClients.jsx
+++ frontend/src/features/clients/indexClients.jsx
@@ -11,15 +11,7 @@ import { Search, ChevronDown } from "lucide-react";
 import ondas from "../../assets/ondasHorizontal.png";
 import Paginator from "../../shared/paginator";
 import { motion, AnimatePresence } from "framer-motion";
-import {
-  showSuccessAlert,
-  showErrorAlert,
-  showConfirmAlert,
-  showWarningAlert,
-  showInfoAlert,
-  showLoadingAlert,
-  showInputAlert,
-} from "../../shared/alerts";
+
 
 export default function IndexClients() {
   const [clients] = useState([
