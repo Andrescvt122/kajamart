36cc96c715ffa2bc499ff97486beae083f7d1712
Andre <andrescvalencia.t@gmail.com>
2026-01-29 20:58:45 -0500

    update

--- frontend/src/shared/components/hooks/returnClients/usePostReturnClients.jsx
+++ frontend/src/shared/components/hooks/returnClients/usePostReturnClients.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/kajamart/api/returnClient";
+const API_URL = "http://localhost:3000/kajamart/api/returnClients";
 
 export const usePostReturnClients = () => {
   const [loading, setLoading] = useState(false);
